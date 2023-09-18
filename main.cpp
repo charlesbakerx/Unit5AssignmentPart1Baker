e09d022616a7ad5e7466c623157636871e64b5d7
charlesbakerx <126838351+charlesbakerx@users.noreply.github.com>
2023-09-18 15:21:17 +0000

    Added tax deductions

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ double weeklyPay(string name, double hoursWorked, double payRate, int empType) {
   double grossPay = 0, netPay = 0;
   // If the employee is full time (1) set the fitRate to 15% otherwise they are part time (2) so we set it to 12%
   double fitRate = (empType == 1) ? 0.15 : 0.12;
+  
   // If the employee worked overtime we calculate 2 times pay on the hours worked over 40
   if (hoursWorked > 40) {
     grossPay = (hoursWorked - 40) * (payRate * 2);
@@ -23,7 +24,14 @@ double weeklyPay(string name, double hoursWorked, double payRate, int empType) {
     hoursWorked = 40;
   } // Now we calculate the base pay and add it to grossPay in the event that the employee worked overtime.
   grossPay += hoursWorked * payRate;
+  // Set netPay to grossPay because FIT is calculated based off of taxable income not gross pay
+  netPay = grossPay;
   // If the employee is full time subtract the $17 and $77 otherwise we don't subtract anything.
-  grossPay -= (empType == 1) ? 94 : 0;
+  netPay -= (empType == 1) ? 94 : 0;
+  // Take out FIT based on taxable income
+  netPay -= (netPay * fitRate);
+  // Take out social security and medicare
+  netPay -= ((grossPay * 0.062) + (grossPay * 0.0145));
   
+  return netPay;
 }
